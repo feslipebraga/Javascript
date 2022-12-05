27fe96a50f76fc04980e62672fede9a958d010c8
feslipebraga <feslipebraga@gmail.com>
2022-12-05 11:59:05 -0300

    first commit

---
+++ exercicios/10.html/acoes.js
@@ -0,0 +1,17 @@
+let acoes = document.querySelector("p#acoes")
+
+function acao1(){
+    acoes.innerHTML += "<p>Ação 1</p>"
+}
+
+function acao2(){
+    acoes.innerHTML += "<p>Ação 2</p>"
+}
+
+function acao3(){
+    acoes.innerHTML += "<p>Ação 3</p>"
+}
+
+function acao4(){
+    acoes.innerHTML += "<p>Ação 4</p>"
+}
